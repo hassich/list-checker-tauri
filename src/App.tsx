2282fb9c0af72c20678b0afd0cab91b9ebfc3ba7
Double-quote <hassich8413@gmail.com>
2026-04-03 06:11:10 +0900

    UPDATE: バージョン番号を更新 1.3.2 -> 1.3.3b Springfest

--- src/App.tsx
+++ src/App.tsx
@@ -690,7 +690,7 @@ function App() {
                   <div className="flex justify-between items-center">
                     <span className="text-gray-600">アプリケーション</span>
                     <span className="font-mono font-semibold text-gray-800">
-                      v1.3.3
+                      v1.3.3b (springfest)
                     </span>
                   </div>
                   <div className="flex justify-between items-center">
